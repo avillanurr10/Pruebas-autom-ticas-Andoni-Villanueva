684b8c01d9363b823511e499b24d51b7660572be
Andoni <149407392+avillanurr10@users.noreply.github.com>
2025-04-11 11:03:27 +0200

    Primer metodo

--- src/test/java/org/example/CuentaTest.java
+++ src/test/java/org/example/CuentaTest.java
@@ -1,8 +1,21 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CuentaTest {
+
+
+    @BeforeEach
+    /*
+    he creado una nueva cuenta y
+    esto asegura que cada test limpio, sin depender de lo que ha pasado  en otros teses.
+    */
+    void prepararCuenta() {
+        Cuenta cuenta = new Cuenta("12345", 100.0);
+    }
+
 
     @org.junit.jupiter.api.Test
     void getNumero() {
